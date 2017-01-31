60d4ae5b788b317eb724fa9c14736fdd7ec41907
Colette-pgm <colette.paugame@gmail.com>
2017-01-31 03:03:58 +0100

    Import

---
+++ Paysages/Assets/GearScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GearScript : MonoBehaviour {
+
+	public GameObject panel;
+
+	void OnTriggerEnter(Collider other) {
+		panel.SetActive (true);
+	}
+
+	void OnTriggerExit(Collider other) {
+		panel.SetActive (false);
+	}
+}
